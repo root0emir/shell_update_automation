e81072972936acfe13d88960a49b3e3fc5924aff
Emir <142451159+root0emir@users.noreply.github.com>
2025-02-13 14:07:15 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# ----------------------------
-# guncelleme otomasyonu root0emir
-# ----------------------------
-
 # log dosyasının konumu
 LOG_FILE="/var/log/system_update.log"
 
@@ -11,7 +7,7 @@ LOG_FILE="/var/log/system_update.log"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
-NC="\033[0m" # No Color
+NC="\033[0m" 
 
 # dagitim tespiti yapma os-release dosyasından 
 detect_distro() {
